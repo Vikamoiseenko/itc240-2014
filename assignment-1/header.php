282066e117c1e4540798cf0f1a1921398a3a304d
Victoria Moiseenko <moiseenkovika@gmail.com>
2014-04-12 18:49:40 -0700

    Change header.php

--- assignment-1/header.php
+++ assignment-1/header.php
@@ -1,9 +1,12 @@
 <!doctype html>
+<html>
 <head>
 <title><?php echo $page_title; ?></title>
 </head>
 <body>
-<h1>INCLUDED FILE! HEADER.PHP! EXCITING!</h1>
-<?php
-echo $page_title;
-?>
+
+<p>This is in the header file</p>
+
+<header>
+My header
+</header>
