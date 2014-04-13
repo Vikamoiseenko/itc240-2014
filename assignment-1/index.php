5b57f8d4c60db3dc94d89b023831b946ca06c9b8
Victoria Moiseenko <moiseenkovika@gmail.com>
2014-04-12 17:42:28 -0700

    change index.php

--- assignment-1/index.php
+++ assignment-1/index.php
@@ -1,2 +1,15 @@
 <?php
+$page_title = "Main page";
+include("header.php");
+?>
+
+This is my first page!
+
+<?php
+//this show debug information on the page
+//phpinfo();
+?>
+
+This is a blank page
+
 
