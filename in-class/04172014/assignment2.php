dbf1b4c848b2337fd55e3489437893a5fdcb726b
Victoria Moiseenko <moiseenkovika@gmail.com>
2014-04-24 20:00:04 -0700

    added

--- in-class/04172014/assignment2.php
+++ in-class/04172014/assignment2.php
@@ -1,53 +1,76 @@
 <!doctype html>
 <html>
+
+<head>
+<title>Assignment 2 - Mad Lib</title>
+</head>
+
+<body>
+<h1>Mad Lib</h1>
+<p>Please, enter the noun, verb, adjective, first number, and second number (between 1-10)</p>
+
 <?php
 
 $method = $_SERVER["REQUEST_METHOD"];
-echo $method;
+
 if ($method == "GET") {
 ?>
-
+<!-- create a form -->
 	<form method="post">
-	<input value="noun" name="noun">
-	<input value="verb" name="verb">
-	<input value="adjective" name="adjective">
-	<input name="number_1" placeholder="number_1">
-	<input name="number_2" placeholder="number_2">
-	<input type="checkbox" value="awesome" name="is_awesome">
-	<label for="is_awesome">Do you want to get a story?</label>
-		<button>POST</button>
+	<label for="noun">Noun</label>
+	<input name="noun" placeholder="noun">
+	
+	<label for="verb">Verb</label>
+	<input name="verb" placeholder="verb">
+	
+	<label for="adjective">Adjective</label>
+	<input name="adjective" placeholder="adjective">
+	
+	<label for="number1">Number</label>
+	<input name="number1" placeholder="number1">
+	
+	<label for="number2">Number</label>
+	<input name="number2" placeholder="number2">
+	
+	<button>GET A STORY</button>
 	</form>
-
 <?php
 } else {
 ?>
-	<pre> <!-- pre format  -->
-	<?php print_r($_REQUEST);?>
-	</pre>
+	<!-- <pre>  pre format  
+	<!-- <?php print_r($_REQUEST);?> -->
+	<!-- </pre> -->
 
-	I buy: <?= $_REQUEST["noun"] ?>
+	<p>My dear Valentine,</p>
+	<p>You are so <b> <?= $_REQUEST["adjective"] ?> </b> and sweet. When I see you, I feel like <b> <?= $_REQUEST["noun"] ?> </b>. You smell like caramel. Would you like to go with me at 11 morning on Valentine's day?. There is a great restaurant there. And very yummy food. There is also a fun center where kids can <b> <?= $_REQUEST["verb"] ?> </b> in the pool, hockey and gumball bowling.</p>
 <?php
+$noun = $_REQUEST["noun"];
 $verb = $_REQUEST["verb"];
-$post_voting = $verb - 18;
+$adjective = $_REQUEST["adjective"];
+$number1 = $_REQUEST["number1"];
+$number2 = $_REQUEST["number2"];
 
-if ($post_voting >= 0) {
+if ($number1 == 5) {
 ?>
-?>
-You have been able to vote for <?= $post_voting ?> years.
+	<p>Would you like to go with me at <b> <?= $_REQUEST["number1"] ?> </b> morning on Valentine's day?. Hope you will be available).  </p>
 
+<?php
+}
+if ($number2 <=10) {
+?> 
+	<p>They serve potato salad, chicken soup, and give <b> <?= $_REQUEST["number2"] ?> </b> Margaritas free!!!! I hope you can come! It would be so great to see you!!!!</p></p>
 <?php
 } else {
 ?>
-You cannot vote!!
-<?php
-}
+<p>Do not come, because they do not serve Ice-cream!!!</p>
 
-if (isset($_REQUEST["is_awesome"])) {
-echo "Also, you are awesome.";
-}
-?>
-<a href="assignment2.php">GET</a>
 <?php
 }
 ?>
+<a href="assignment2.php">Try one mote time</a>
+<?php
+}
+?>
+</body>
+
 </html>
